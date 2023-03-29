f7d61c1d94db28e7508bb52d2a913c45c6cf8a6b
Shedrack akintayo <akintayoshedrack@gmail.com>
2023-03-29 23:47:02 +0100

    set up project

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+
+     <p>Hello world</p>
+    </div>
+  )
+}
+
+export default App
