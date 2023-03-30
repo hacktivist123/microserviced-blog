320c0c6f35b8bf40213b644cd515394cd11b0b57
Shedrack akintayo <akintayoshedrack@gmail.com>
2023-03-30 02:27:47 +0100

    added logic for rendering posts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import PostCreate from "./components/PostCreate"
+import PostList from "./components/PostList"
 
 function App() {
 
@@ -7,6 +8,9 @@ function App() {
       <div className="container mx-auto">
       <h1 className="text-4xl py-3"> Create New Post</h1>
       <PostCreate/>
+      <hr className=" mt-5 px-3"/>
+      <h1 className="text-3xl py-3">Posts</h1>
+      <PostList/>
       </div>
 
     </div>
---
+++ client/src/components/PostList.jsx
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
+
+const PostList = () => {
+  const [posts, setPosts] = useState({})
+
+  const fetchPosts = async () => {
+    const res = await axios.get ('http://localhost:4000/posts');
+    setPosts(res.data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const renderedPosts = Object.values(posts).map(post => (
+    <div className='max-w-md lg:max-w-full lg:flex rounded-md w-1/3 mb-5 px-5 py-5 border' key={post.id}>
+      <div>
+        <h3>{post.title}</h3>
+      </div>
+    </div>
+  ));
+  return (
+    <div className='flex flex-row flex-wrap justify-between'>
+      {renderedPosts}
+    </div>
+  )
+}
+
+export default PostList
